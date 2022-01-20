101073fc59964ff0326e5fcb2a88ace632a5ff97
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2022-01-20 16:21:04 +0200

    changed version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Vitaman02",
     url="https://github.com/Vitaman02/pygsearch",
     project_urls={},
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(),
     license="MIT",
     description="Python library to get google search results.",
